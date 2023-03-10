b6504e1efa5db12110260a2f1341c545f6e55221
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-03-09 23:27:15 -0300

    arrumando classe

--- src/Atividade2.java
+++ src/Atividade2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ativiade2 {
+public class Atividade2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numeroEscolhido, contador = 0, soma = 0;
